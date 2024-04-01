8e27ed63c59dc5082414bea32f3e0d40bc4e272f
enricooxd <160501359+enricooxd@users.noreply.github.com>
2024-04-01 13:46:16 +0000

    commit mais recente

---
+++ praticas/praticas1/questao7.c
@@ -0,0 +1,12 @@
+//faca um programa em c que calcule a altura alcançada por um avião após ter percorrido uma certa distancia(seno(angulo)= altura/distancia). Considere o angulo de inclinaçao do avião menor ou igual a 45°
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+  float distancia;
+  float angulo;
+  const double PI = 3.14;
+
+
+  return 0,
+}
