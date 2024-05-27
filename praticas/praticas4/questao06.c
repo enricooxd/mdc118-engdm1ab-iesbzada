84ce557fa6d466432cefc3d3d97016e29608aa9c
enricooxd <160501359+enricooxd@users.noreply.github.com>
2024-05-27 11:51:28 +0000

    commit top

--- praticas/praticas4/questao06.c
+++ praticas/praticas4/questao06.c
@@ -1,5 +1,9 @@
+/*Faça um programa em C que leia um verbo e imprima a conjugação no presente do indicativo caso ele termine em AR (ex.: PROGRAMAR, EU PROGRAMO, TU PROGRAMAS, ELE PROGRAMA, NÓS PROGRAMAMOS, VÓS PROGRAMAIS E ELES PROGRAMAM).
+*/  
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 int main(){
   char verbo[21];
 
@@ -7,6 +11,11 @@ int main(){
   int deu_certo = scanf("%s", verbo);
 
   int tamanho = strlen(verbo);
+
+  for(int i=0; i<tamanho; i++) {
+    verbo[i] = toupper(verbo[i]); // tolower(verbo[i]);
+  }
+  
   int ultima_posicao = tamanho - 1;
   int penultima_posicao = ultima_posicao - 1;
 
